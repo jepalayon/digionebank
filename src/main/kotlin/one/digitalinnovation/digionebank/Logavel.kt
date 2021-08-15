f2cdb665fd64898c331ac4df42f8e8f09b4506bf
JEPAlayon <jepale@hotmail.com>
2021-08-15 12:24:22 -0300

    Classe Cliente-Polimorfismo

---
+++ src/main/kotlin/one/digitalinnovation/digionebank/Logavel.kt
@@ -0,0 +1,5 @@
+package one.digitalinnovation.digionebank
+
+abstract interface Logavel {
+    fun login(): Boolean
+}
--- src/main/kotlin/one/digitalinnovation/digionebank/testes/TesteFuncionarioGerente.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/testes/TesteFuncionarioGerente.kt
@@ -5,7 +5,9 @@ import one.digitalinnovation.digionebank.Funcionario
 import one.digitalinnovation.digionebank.Gerente
 
 fun main(){
-    val maria = Gerente(nome = "Maria do Carmo", cpf = "123654987-52", salario = 5000.0)
+    val maria = Gerente(nome = "Maria do Carmo", cpf = "123654987-52", salario = 5000.0, senha = "senha123")
     ImprimeRelatorioFuncionario.imprime(maria)
-    }
+
+    TesteAutenticacao().autentica(maria)
+}
 
