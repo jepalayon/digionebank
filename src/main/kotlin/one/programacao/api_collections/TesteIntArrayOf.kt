e49afe60a3c02f5d698ee1333411f472a8b0032e
JEPAlayon <jepale@hotmail.com>
2021-09-21 20:44:06 -0300

    Int Array Of

---
+++ src/main/kotlin/one/programacao/api_collections/TesteIntArrayOf.kt
@@ -0,0 +1,18 @@
+package one.programacao.api_collections
+
+fun main(){
+    val values = intArrayOf(2,3,4,1,10,7)
+
+    values.forEach {
+        println(it)
+    }
+
+    println("===================================")
+
+    values.sort()
+    values.forEach {
+        println(it)
+    }
+
+
+}
